701fafeaec1b4b477c4ad66c3d5dd842c6b11e5b
Manuel Santos <manueljoaosantos@gmail.com>
2023-10-09 17:30:17 +0100

    feat: add application persistence

---
+++ Mjos.Clean.Persistence/Repositories/ClubRepository.cs
@@ -0,0 +1,24 @@
+﻿using Mjos.Clean.Application.Interfaces.Repositories;
+using Mjos.Clean.Domain.Entities;
+using Mjos.Clean.Persistence.Contexts;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mjos.Clean.Persistence.Repositories
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly IGenericRepository<Club> _repository;
+
+        public ClubRepository(IGenericRepository<Club> repository) 
+        {
+            _repository = repository;
+        } 
+    }
+}
---
+++ Mjos.Clean.Persistence/Repositories/PlayerRepository.cs
@@ -0,0 +1,29 @@
+﻿using Mjos.Clean.Application.Interfaces.Repositories;
+using Mjos.Clean.Domain.Entities;
+using Mjos.Clean.Persistence.Contexts;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mjos.Clean.Persistence.Repositories
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        private readonly IGenericRepository<Player> _repository;
+
+        public PlayerRepository(IGenericRepository<Player> repository) 
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<Player>> GetPlayersByClubAsync(int clubId)
+        {
+            return await _repository.Entities.Where(x => x.ClubId == clubId).ToListAsync();
+        }
+    }
+}
